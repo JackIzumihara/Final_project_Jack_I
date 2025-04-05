1c7e41a644d3329acc8443578030e9927b2f4e1f
JackIzumihara <1551500700@k12.hi.us>
2025-04-05 21:21:41 +0000

    Tried to make multiplier + added fixed block click

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-let score = 1;
+let score = 0;
+let upgrade = 0;
+const scoreDisplay = document.getElementById('score');
+const clickerButton = document.getElementById('clicker');
+const upgradeDisplay = document.getElementById('Upgrade1');
+const upgrade1button = document.getElementById('upgrade1button');
+clickerButton.addEventListener('click', function() {
+   score+=1
+    scoreDisplay.textContent = "Score: " + (score + upgrade);
+    console.log("Current Score" + score + upgrade);
 
-function AddScore(score){
-    score += 1;
-    console.log(score);
-}
+  });
+upgrade1button.addEventListener('click', function(){
+    upgrade +=1;
+    upgradeDisplay.textContent = "Upgrade 1 level:" + upgrade;
+    console.log("Upgrade 1 level")
+})
 
 MainButton.addEventListener("click", function(){
     AddScore();
+
+    Upgrade1.addEventListener('click', function(){
+        AddUpgrade();
+    })
 })
